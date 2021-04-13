2b3bc0801f230558cc5f5e97943381e44f9d0c83
serafla <63385649+serafla@users.noreply.github.com>
2021-04-13 16:26:11 -0400

    added ease in's

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,11 @@
-import { gsap } from "gsap";
+import { Bounce, gsap, Power4 } from "gsap";
 
 gsap.from("#egg", { duration: 3, x: -500});
 gsap.to("#egg", {duration: 5.35, x: 360, rotation:740, scale: 2.5 })
 gsap.to("#egg", {duration: 1.5, rotation: 700, delay:5.35})
 gsap.to("#egg", {duration: 2, rotation: 720, delay:7.35})
 gsap.to("#egg", {duration: 1, y: 30, scaleY: 2, scaleX: 3, delay:9.35})
-gsap.to("#egg", {duration: 0.25, y: -150, delay:10.35, scale: 1.5, rotation: 180})
+gsap.to("#egg", {duration: 0.25, y: -150, delay:10.35, scale: 1.5, rotation: 180, ease: Power4.easeOut})
 gsap.to("#egg", {duration: 0.5, y: -200, delay:10.60, scale: 1, rotation: 360})
 gsap.to("#egg", {duration: 1, y: -200, delay:11.10, scaleY: 0.75, scaleX: 1.25})
 gsap.to("#egg", {duration: 0.5, y: 100, delay:12.10, scale: 2.5 })
